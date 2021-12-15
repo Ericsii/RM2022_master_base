c596182169bcd0406c878f4f5a4e23de9911d68f
冯云龙 <ericc_feng@qq.com>
2021-12-15 16:16:11 +0800

    feat(rm_cam): 加入WrapperClient同步接受topic
    Former-commit-id: d27aca75bd749bea4b7772d0c33d582c235ac7a7 [formerly 94d3b23a20564de07992516483534a3dcd7f6dcb]
    Former-commit-id: 48e84a96e99e2552e64e45b3dd24cd137e671112
    Former-commit-id: a453f7543b34f1b8082abe992879a404668e6969

--- rm_cam/src/cam_client.cpp
+++ rm_cam/src/cam_client.cpp
@@ -8,125 +8,125 @@
 #include "rm_interfaces/srv/get_camera_info.hpp"
 #include "rm_interfaces/qos_policy.hpp"
 
-
 namespace rm_cam
 {
-CamClient::CamClient(
-    rclcpp::Node::SharedPtr node,
-    const std::string &camera_name,
-    CallBack process_fn,
-    bool spin_thread
-): node_(node), camera_name_(camera_name), process_fn_(process_fn), spin_thread_(spin_thread)
-{
-    using namespace std::placeholders;
-    node_->declare_parameter("best_effort_qos", false);
-    auto best_effort_qos = node_->get_parameter("best_effort_qos").as_bool();
-
-    if (spin_thread_)
+    CamClient::CamClient(
+        rclcpp::Node::SharedPtr node,
+        const std::string &camera_name,
+        CallBack process_fn,
+        bool spin_thread) : node_(node), camera_name_(camera_name), process_fn_(process_fn), spin_thread_(spin_thread)
     {
-        callback_group_ = node_->create_callback_group(
+        using namespace std::placeholders;
+        node_->declare_parameter("best_effort_qos", false);
+        auto best_effort_qos = node_->get_parameter("best_effort_qos").as_bool();
+
+        if (spin_thread_)
+        {
+            callback_group_ = node_->create_callback_group(
+                rclcpp::CallbackGroupType::MutuallyExclusive, false);
+            auto sub_opt = rclcpp::SubscriptionOptions();
+            sub_opt.callback_group = callback_group_;
+
+            if (best_effort_qos)
+            {
+                // 图像传输QoS配置
+                rclcpp::QoS img_pub_qos_profile(rclcpp::KeepLast(1), best_effort_qos_policy);
+                img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
+                    camera_name_ + "/image_raw", img_pub_qos_profile, std::bind(&CamClient::img_cb, this, _1), sub_opt);
+            }
+            else
+            {
+                img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
+                    camera_name_ + "/image_raw", 1, std::bind(&CamClient::img_cb, this, _1), sub_opt);
+            }
+
+            executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+            executor_->add_callback_group(callback_group_, node_->get_node_base_interface());
+            executor_thread_ = std::make_unique<std::thread>([&]()
+                                                             { executor_->spin(); });
+        }
+        else
+        {
+            if (best_effort_qos)
+            {
+                // 图像传输QoS配置
+                rclcpp::QoS img_pub_qos_profile(rclcpp::KeepLast(1), best_effort_qos_policy);
+                img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
+                    camera_name_ + "/image_raw", img_pub_qos_profile, std::bind(&CamClient::img_cb, this, _1));
+            }
+            else
+            {
+                img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
+                    camera_name_ + "/image_raw", 1, std::bind(&CamClient::img_cb, this, _1));
+            }
+        }
+    }
+
+    CamClient::~CamClient()
+    {
+        if (spin_thread_)
+        {
+            executor_->cancel();
+            executor_thread_->join();
+        }
+    }
+
+    void CamClient::img_cb(const sensor_msgs::msg::Image::ConstSharedPtr msg)
+    {
+        if (run_flag_)
+        {
+            auto img = cv_bridge::toCvShare(msg, "bgr8")->image.clone();
+            auto img_stamp = msg->header.stamp.sec + 0.000000001 * msg->header.stamp.nanosec;
+            process_fn_(img, img_stamp);
+        }
+    }
+
+    bool CamClient::get_camera_info(sensor_msgs::msg::CameraInfo &info)
+    {
+        using namespace std::chrono_literals;
+        auto callback_group = node_->create_callback_group(
             rclcpp::CallbackGroupType::MutuallyExclusive, false);
-        auto sub_opt = rclcpp::SubscriptionOptions();
-        sub_opt.callback_group = callback_group_;
+        auto exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+        auto client = node_->create_client<rm_interfaces::srv::GetCameraInfo>(
+            camera_name_, rmw_qos_profile_services_default, callback_group);
+        exec->add_callback_group(callback_group, node_->get_node_base_interface());
 
-        if (best_effort_qos)
+        while (!client->wait_for_service(std::chrono::seconds(1)))
         {
-            // 图像传输QoS配置
-            rclcpp::QoS img_pub_qos_profile(rclcpp::KeepLast(1), best_effort_qos_policy);
-            img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
-            camera_name_ + "/image_raw", img_pub_qos_profile, std::bind(&CamClient::img_cb, this, _1), sub_opt);
+            if (!rclcpp::ok())
+            {
+                RCLCPP_ERROR(
+                    node_->get_logger(),
+                    "[get_camera_info] client interrupted!");
+                return false;
+            }
         }
-        else {
-            img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
-            camera_name_ + "/image_raw", 1, std::bind(&CamClient::img_cb, this, _1), sub_opt);
-        }
-        
-        executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-        executor_->add_callback_group(callback_group_, node_->get_node_base_interface());
-        executor_thread_ = std::make_unique<std::thread>([&](){executor_->spin();});
-    }
-    else {
-        if (best_effort_qos)
-        {
-            // 图像传输QoS配置
-            rclcpp::QoS img_pub_qos_profile(rclcpp::KeepLast(1), best_effort_qos_policy);
-            img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
-            camera_name_ + "/image_raw", img_pub_qos_profile, std::bind(&CamClient::img_cb, this, _1));
-        }
-        else {
-            img_sub_ = node_->create_subscription<sensor_msgs::msg::Image>(
-            camera_name_ + "/image_raw", 1, std::bind(&CamClient::img_cb, this, _1));
-        }
-    }
-}
-
-CamClient::~CamClient()
-{
-    if(spin_thread_)
-    {
-        executor_->cancel();
-        executor_thread_->join();
-    }
-}
-
-void CamClient::img_cb(const sensor_msgs::msg::Image::ConstSharedPtr msg)
-{
-    if (run_flag_)
-    {
-        auto img = cv_bridge::toCvShare(msg, "bgr8")->image.clone();
-        auto img_stamp = msg->header.stamp.sec + 0.000000001 * msg->header.stamp.nanosec;
-        process_fn_(img, img_stamp);
-    }
-}
-
-bool CamClient::get_camera_info(sensor_msgs::msg::CameraInfo &info)
-{
-    using namespace std::chrono_literals;
-    auto callback_group = node_->create_callback_group(
-        rclcpp::CallbackGroupType::MutuallyExclusive, false);
-    auto exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-    auto client = node_->create_client<rm_interfaces::srv::GetCameraInfo>(
-        camera_name_, rmw_qos_profile_services_default, callback_group
-    );
-    exec->add_callback_group(callback_group, node_->get_node_base_interface());
-    
-    while(!client->wait_for_service(std::chrono::seconds(1)))
-    {
-        if (!rclcpp::ok())
+        auto request = std::make_shared<rm_interfaces::srv::GetCameraInfo::Request>();
+        auto result_future = client->async_send_request(request);
+        if (exec->spin_until_future_complete(result_future, 5s) !=
+            rclcpp::FutureReturnCode::SUCCESS)
         {
             RCLCPP_ERROR(
                 node_->get_logger(),
-                "[get_camera_info] client interrupted!"
-            );
+                "[get_camera_info] service timeout!");
+            return false;
+        }
+
+        auto result = result_future.get();
+        if (result->success)
+        {
+            info = result->camera_info;
+            return true;
+        }
+        else
+        {
+            RCLCPP_ERROR(node_->get_logger(), "[get_camera_info] service call failed.");
             return false;
         }
     }
-    auto request = std::make_shared<rm_interfaces::srv::GetCameraInfo::Request>();
-    auto result_future = client->async_send_request(request);
-    if (exec->spin_until_future_complete(result_future, 5s) != 
-        rclcpp::FutureReturnCode::SUCCESS)
-    {
-        RCLCPP_ERROR(
-            node_->get_logger(),
-            "[get_camera_info] service timeout!"
-        );
-        return false;
-    }
 
-    auto result = result_future.get();
-    if (result->success)
-    {
-        info = result->camera_info;
-        return true;
-    }
-    else {
-        RCLCPP_ERROR(node_->get_logger(), "[get_camera_info] service call failed.");
-        return false;
-    }
-}
+    void CamClient::start() { run_flag_ = true; }
 
-void CamClient::start() {run_flag_ = true;}
-
-void CamClient::stop() {run_flag_ = false;}
+    void CamClient::stop() { run_flag_ = false; }
 
 } // namespace rm_cam
---
+++ rm_cam/src/wrapper_client.cpp
@@ -0,0 +1,104 @@
+#include "rm_cam/wrapper_client.hpp"
+
+#include <memory>
+#include <string>
+#include <chrono>
+
+#include "cv_bridge/cv_bridge.h"
+#include "rm_interfaces/srv/get_camera_info.hpp"
+#include "rm_interfaces/qos_policy.hpp"
+
+namespace rm_cam
+{
+    WrapperClient::WrapperClient(
+        rclcpp::Node::SharedPtr node,
+        const std::string &camera_name,
+        const std::string &imu_name,
+        DataCallBack process_fn) : node_(node), camera_name_(camera_name), imu_name_(imu_name), process_fn_(process_fn)
+    {
+        using std::placeholders::_1;
+        using std::placeholders::_2;
+
+        node_->declare_parameter("best_effort_qos", false);
+        auto best_effort_qos = node_->get_parameter("best_effort_qos").as_bool();
+
+        if (best_effort_qos)
+        {
+            // 图像传输配置
+            rclcpp::QoS img_sub_qos_profile(rclcpp::KeepLast(1), best_effort_qos_policy);
+            img_sub_.subscribe(node_, camera_name_ + "/image_raw", img_sub_qos_profile.get_rmw_qos_profile());
+
+            // imu传输QoS配置
+            rclcpp::QoS imu_sub_qos_profile(rclcpp::KeepLast(10), best_effort_qos_policy);
+            imu_sub_.subscribe(node_, imu_name_, imu_sub_qos_profile.get_rmw_qos_profile());
+        }
+        else
+        {
+            img_sub_.subscribe(node_, camera_name_ + "/image_raw");
+            imu_sub_.subscribe(node_, imu_name_);
+        }
+
+        sync_ = std::make_shared<message_filters::Synchronizer<ApproximatePolicy>>(ApproximatePolicy(5), img_sub_, imu_sub_);
+        sync_->registerCallback(std::bind(&WrapperClient::data_cbk, this, _1, _2));
+    }
+
+    void WrapperClient::data_cbk(const sensor_msgs::msg::Image::ConstSharedPtr img,
+                                 const geometry_msgs::msg::PoseStamped::ConstSharedPtr pose)
+    {
+        if (run_flag_)
+        {
+            auto c_header = img->header;
+            auto c_img = cv_bridge::toCvShare(img, "bgr8")->image.clone();
+            auto c_pose = pose->pose;
+            process_fn_(c_header, c_img, c_pose);
+        }
+    }
+
+    bool WrapperClient::get_camera_info(sensor_msgs::msg::CameraInfo &info)
+    {
+        using namespace std::chrono_literals;
+        auto callback_group = node_->create_callback_group(
+            rclcpp::CallbackGroupType::MutuallyExclusive, false);
+        auto exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+        auto client = node_->create_client<rm_interfaces::srv::GetCameraInfo>(
+            camera_name_, rmw_qos_profile_services_default, callback_group);
+        exec->add_callback_group(callback_group, node_->get_node_base_interface());
+
+        while (!client->wait_for_service(std::chrono::seconds(1)))
+        {
+            if (!rclcpp::ok())
+            {
+                RCLCPP_ERROR(
+                    node_->get_logger(),
+                    "[get_camera_info] client interrupted!");
+                return false;
+            }
+        }
+        auto request = std::make_shared<rm_interfaces::srv::GetCameraInfo::Request>();
+        auto result_future = client->async_send_request(request);
+        if (exec->spin_until_future_complete(result_future, 5s) !=
+            rclcpp::FutureReturnCode::SUCCESS)
+        {
+            RCLCPP_ERROR(
+                node_->get_logger(),
+                "[get_camera_info] service timeout!");
+            return false;
+        }
+
+        auto result = result_future.get();
+        if (result->success)
+        {
+            info = result->camera_info;
+            return true;
+        }
+        else
+        {
+            RCLCPP_ERROR(node_->get_logger(), "[get_camera_info] service call failed.");
+            return false;
+        }
+    }
+
+    void WrapperClient::start() { run_flag_ = true; }
+
+    void WrapperClient::stop() { run_flag_ = false; }
+} // namespace rm_cam
